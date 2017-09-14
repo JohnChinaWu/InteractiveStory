0a6017e3e97e10aa99afc754f3f22515899347b7
wuwenjun <johnwu1016@gmail.com>
2017-09-14 10:40:09 +0800

    get data from an intent

---
+++ app/src/main/java/com/johnwu1016/interactivestory/StoryActivity.java
@@ -0,0 +1,19 @@
+package com.johnwu1016.interactivestory;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class StoryActivity extends AppCompatActivity {
+    public static String TAG = StoryActivity.class.getSimpleName();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+        Log.d(TAG, name);
+    }
+}
