908c6d6f21fb8a684a9dae7bb9819db164f53cf0
wuwenjun <johnwu1016@gmail.com>
2017-09-14 10:08:58 +0800

    get a text from TextView

--- app/src/main/java/com/johnwu1016/interactivestory/MainActivity.java
+++ app/src/main/java/com/johnwu1016/interactivestory/MainActivity.java
@@ -2,12 +2,30 @@ package com.johnwu1016.interactivestory;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView nameFiled;
+    private Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nameFiled = (TextView) findViewById(R.id.nameEditText);
+        startButton = (Button) findViewById(R.id.startButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = nameFiled.getText().toString();
+                Toast.makeText(MainActivity.this, name, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
