2259b3e48724d84269eaa522657f0c3662d2c88d
wuwenjun <johnwu1016@gmail.com>
2017-09-19 10:11:11 +0800

    load the first page

--- app/src/main/java/com/johnwu1016/interactivestory/ui/StoryActivity.java
+++ app/src/main/java/com/johnwu1016/interactivestory/ui/StoryActivity.java
@@ -1,18 +1,35 @@
 package com.johnwu1016.interactivestory.ui;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.johnwu1016.interactivestory.R;
+import com.johnwu1016.interactivestory.model.Page;
+import com.johnwu1016.interactivestory.model.Story;
 
 public class StoryActivity extends AppCompatActivity {
     public static String TAG = StoryActivity.class.getSimpleName();
+    private Story story;
+    private ImageView storyImageView;
+    private TextView storyTextView;
+    private Button choice1Button;
+    private Button choice2Button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story);
+        storyImageView = (ImageView) findViewById(R.id.storyImageView);
+        storyTextView = (TextView)findViewById(R.id.storyTextView);
+        choice1Button = (Button)findViewById(R.id.choice1Button);
+        choice2Button = (Button)findViewById(R.id.choice2Button);
 
         Intent intent = getIntent();
         String name = intent.getStringExtra(getString(R.string.key_name));
@@ -20,5 +37,17 @@ public class StoryActivity extends AppCompatActivity {
         if (name == null || name.isEmpty())
             name = "Friend";
         Log.d(TAG, name);
+
+        story = new Story();
+        loadPage(0);
     }
+
+    private void loadPage(int pageNumber) {
+        Page page = story.getPage(pageNumber);
+
+        Drawable image = ContextCompat.getDrawable(this, page.getImageId());
+        storyImageView.setImageDrawable(image);
+    }
+
+
 }
